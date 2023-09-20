82c33a925055edb03ef5a5212c0711f9a47969c3
Sean <ravenrcft@users.noreply.github.com>
2023-09-19 20:50:27 -0400

    Update ispali.lua
    Touch up divlength

--- ispali.lua
+++ ispali.lua
@@ -1,8 +1,8 @@
-function ispali(word)
+function ispali(word) 
   word = string.lower(word)
-  wordlength = #word; divlength = math.floor(wordlength / 2)
-  if string.sub(word, 1, divlength) == (string.reverse(string.sub(word, -divlength, -1))) then
-    return true
-  end
-  return false
+  divlength = math.floor(#word / 2) 
+  if string.sub(word, 1, divlength) == string.reverse(string.sub(word, -divlength, -1)) then 
+    return true 
+  end 
+  return false 
 end
