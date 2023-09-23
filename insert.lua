8ded330e963c697472616d7832e8f330b9787412
Sean <ravenrcft@users.noreply.github.com>
2023-09-23 16:14:10 -0400

    Update insert.lua

--- insert.lua
+++ insert.lua
@@ -3,12 +3,7 @@
 function insert(origtext, index, addtext)
   local pretext = string.sub(origtext,  1, index - 1)
   local posttext = string.sub(origtext, index)
-  
-  if index > 1 then
-    result = pretext .. addtext .. posttext
-    return result
-  else
-    result = addtext .. posttext
-    return result
-  end
+
+  local result = pretext .. addtext .. posttext
+  return result
 end
