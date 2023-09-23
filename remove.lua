061303dd516193450b91dc872dbde3a6fd919e1c
Sean <ravenrcft@users.noreply.github.com>
2023-09-23 15:10:32 -0400

    Create remove.lua

---
+++ remove.lua
@@ -0,0 +1,8 @@
+--lua 5.3.5 
+
+function remove(text, index, incrementing)
+  local pretext = string.sub(text, 1, index - 1)
+  local posttext = string.sub(text, index + incrementing, #text)
+  local result = pretext .. posttext
+  return result
+end
