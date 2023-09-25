316fcbd030c4901ce2cebb5662a68ebf433ad134
Sean <ravenrcft@users.noreply.github.com>
2023-09-25 13:04:37 -0400

    Update remove.lua

--- remove.lua
+++ remove.lua
@@ -3,6 +3,5 @@
 function remove(text, index, incrementing)
   local pretext = string.sub(text, 1, index - 1)
   local posttext = string.sub(text, index + incrementing, #text)
-  local result = pretext .. posttext
-  return result
+  return pretext .. posttext
 end
